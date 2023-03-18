eaa22d5d787462fb07300ad618484079b5d06bef
Kerem <85187618+Keremy-PRO@users.noreply.github.com>
2023-03-18 11:30:07 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var mario_img;
+function preload(){
+    mario_img=loadAnimation("imgs/mario/mario01.png","imgs/mario/mario02.png","imgs/mario/mario03.png");
+}
+
+function setup(){
+canvas=createCanvas(600,300);
+}
+
+function draw(){
+background("blue");
+}
